78acb6af156965b552a8bc940a2119a7d7800c15
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-05-17 10:11:55 +0200

    Displays begin
    Add a test files which I use as reference.
    Add the pdf of the project in the folder
    Add displays.py which I use to displays everythings in the project.

---
+++ codefiles/displays.py
@@ -0,0 +1,88 @@
+"""
+This files contain all the displays variables and functions.
+"""
+
+# --- Import ---
+from tkinter import *
+from random import randrange
+
+# --- Constant delcare ---
+
+PANTEXTSIZE = 100
+PANTEXTCOLOR = PANTEXTCOLOR
+PANCOLOR = 'red'
+BUTTONCOLOR = BUTTONCOLOR
+BUTTONTEXTCOLOR = BUTTONTEXTCOLOR
+
+
+# --- Functions declare ---
+def clickHelp():
+
+    fenHelp = Tk()
+    labHelp = Label(fenHelp, text="""
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    You need some help ?
+
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """, fg=PANTEXTCOLOR, background='red', font=PANTEXTSIZE)
+    labHelp.pack()
+    boutQuit = Button(fenHelp, text='Quit', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = fenHelp.destroy)
+    boutQuit.pack(side=RIGHT)
+
+    labHelp.mainloop()
+
+def reserve_place():
+    nbr = randrange(100)
+
+    fenRes = Tk()
+    labRes = Label(fenRes, text="""
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    The place {} is reserved for you !
+
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """.format(nbr), fg=PANTEXTCOLOR, background='red', font=PANTEXTSIZE)
+    labRes.pack()
+    boutQuit = Button(fenRes, text='Quit', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = fenRes.destroy)
+    boutQuit.pack(side=RIGHT)
+
+    fenRes.mainloop()
+
+# --- All the windows declare ---
+fenMenu1 = Tk()
+
+# --- Label declare ---
+labWelc = Label(fenMenu1, text="""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+Car Park Planer
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""", fg=PANTEXTCOLOR, background=PANCOLOR, font=PANTEXTSIZE)
+
+# Button declare
+boutQuit = Button(fenMenu1, text='Quit', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = fenMenu1.destroy)
+boutHelp = Button(fenMenu1, text='Help', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = clickHelp)
+boutReserved = Button(fenMenu1, text='Find a place', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = reserve_place)
+
+###############################################################################
+
+if __name__ == '__main__':
+    labWelc.pack()
+    boutQuit.pack(side=RIGHT)
+    boutHelp.pack(side=RIGHT)
+    boutReserved.pack(side=LEFT)
+    fenMenu1.mainloop()
