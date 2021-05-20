4df7ff7b56e92992093ae239a5c8db02b91e6c5b
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-05-20 21:05:37 +0200

    Upgrades on displays
    Add constantes.py for declare all function in the program.
    Reorgenize the structure of the displays.py file

--- codefiles/displays.py
+++ codefiles/displays.py
@@ -2,89 +2,99 @@
 This files contain all the displays variables and functions.
 """
 
+###############################################################################
+
+
 # --- Import ---
 from tkinter import *
-from random import randrange
+from random import randrange    # Only as test can be removed
+RAND_NBR = randrange(100)
+from constantes import *
 
-# --- Constant delcare ---
+###############################################################################
 
-PANTEXTSIZE = 100
-PANTEXTCOLOR = 'white'
-PANCOLOR = 'red'
-BUTTONCOLOR = 'orange'
-BUTTONTEXTCOLOR = 'black'
+# --- All the windows declare ---
+window = Tk()
+
+
+# --- Label Menu declare ---
+labWelcMenu = Label(
+    window, text=LABEL_MENU_WINDOW, fg=PANEL_TEXT_COLOR,
+    background=PANEL_COLOR, font=PANEL_TEXT_SIZE
+)
+
+# --- Button Menu declare ---
+boutQuitMenu = Button(
+    window, text='Quit', fg=BUTTON_TEXT_COLOR, background=BUTTON_COLOR,
+    command = window.destroy
+)
+boutHelpMenu = Button(
+    window, text='Help', fg=BUTTON_TEXT_COLOR, background=BUTTON_COLOR,
+    command = lambda:[pack_all('menu', bol=False),pack_all('help')]
+)
+boutReservedMenu = Button(
+    window, text='Find a place', fg=BUTTON_TEXT_COLOR, background=BUTTON_COLOR,
+    command = lambda:[pack_all('menu', bol=False),pack_all('resv')]
+)
+
+# --- Label Help declare ---
+labHelp = Label(
+    window, text=LABEL_HELP_WINDOW, fg=PANEL_TEXT_COLOR,
+    background=PANEL_COLOR, font=PANEL_TEXT_SIZE
+)
+
+# --- Button Help declare ---
+boutQuitHelp = Button(
+    window, text='Quit', fg=BUTTON_TEXT_COLOR, background=BUTTON_COLOR,
+    command = lambda:[pack_all('help', bol=False),pack_all('menu')]
+)
+
+# --- Label Reserved declare ---
+labResv = Label(
+    window, text=LABEL_RESERVED_WINDOW.format(RAND_NBR),
+    fg=PANEL_TEXT_COLOR, background=PANEL_COLOR, font=PANEL_TEXT_SIZE
+)
+
+# --- Button Reserved declare ---
+boutQuitResv = Button(
+    window, text='Quit', fg=BUTTON_TEXT_COLOR, background=BUTTON_COLOR,
+    command = lambda:[pack_all('resv', bol=False),pack_all('menu')]
+)
+
+###############################################################################
 
 
 # --- Functions declare ---
-def clickHelp():
+def pack_all(win, bol=True):
 
-    fenHelp = Tk()
-    labHelp = Label(fenHelp, text="""
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-    You need some help ?
-
-
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    """, fg=PANTEXTCOLOR, background='red', font=PANTEXTSIZE)
-    labHelp.pack()
-    boutQuit = Button(fenHelp, text='Quit', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = fenHelp.destroy)
-    boutQuit.pack(side=RIGHT)
-
-    labHelp.mainloop()
-
-def reserve_place():
-    nbr = randrange(100)
-
-    fenRes = Tk()
-    labRes = Label(fenRes, text="""
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-    The place {} is reserved for you !
-
-
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    """.format(nbr), fg=PANTEXTCOLOR, background='red', font=PANTEXTSIZE)
-    labRes.pack()
-    boutQuit = Button(fenRes, text='Quit', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = fenRes.destroy)
-    boutQuit.pack(side=RIGHT)
-
-    fenRes.mainloop()
-
-# --- All the windows declare ---
-fenMenu1 = Tk()
-
-# --- Label declare ---
-labWelc = Label(fenMenu1, text="""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-Car Park Planer
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-""", fg=PANTEXTCOLOR, background=PANCOLOR, font=PANTEXTSIZE)
-
-# Button declare
-boutQuit = Button(
-    fenMenu1, text='Quit', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = fenMenu1.destroy
-)
-boutHelp = Button(fenMenu1, text='Help', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = clickHelp)
-boutReserved = Button(fenMenu1, text='Find a place', fg=BUTTONTEXTCOLOR, background=BUTTONCOLOR, command = reserve_place)
+    if win == 'menu':
+        if bol == True:
+            labWelcMenu.pack()
+            boutQuitMenu.pack(side=RIGHT)
+            boutHelpMenu.pack(side=RIGHT)
+            boutReservedMenu.pack(side=LEFT)
+        else:
+            labWelcMenu.pack_forget()
+            boutQuitMenu.pack_forget()
+            boutHelpMenu.pack_forget()
+            boutReservedMenu.pack_forget()
+    elif win == 'help':
+        if bol == True:
+            labHelp.pack()
+            boutQuitHelp.pack(side=RIGHT)
+        else:
+            labHelp.pack_forget()
+            boutQuitHelp.pack_forget()
+    elif win == 'resv':
+        if bol == True:
+            labResv.pack()
+            boutQuitResv.pack(side=RIGHT)
+        else:
+            labResv.pack_forget()
+            boutQuitResv.pack_forget()
 
 ###############################################################################
 
 if __name__ == '__main__':
-    labWelc.pack()
-    boutQuit.pack(side=RIGHT)
-    boutHelp.pack(side=RIGHT)
-    boutReserved.pack(side=LEFT)
-    fenMenu1.mainloop()
+    pack_all('menu')
+    window.mainloop()
