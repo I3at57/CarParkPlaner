054021e48b3a1f4312b7d42cdb89535207b9a69f
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-06-05 20:06:20 +0200

    move to an other branch

--- codefiles/declares.py
+++
@@ -1,8 +0,0 @@
-"""
-This file contain all the tkinter object in the project.
-Here are decleare all object that use library.
-"""
-
-###############################################################################
-
-from constantes import *
